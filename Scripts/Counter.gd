a59fc3a7af180a7c743923731796172d50987dee
Aiden <aidenbuttinez@gmail.com>
2025-10-08 23:13:14 -0500

    recode part 1.2
    worked on desktop, added Clicks per second and some other shiz. -potato :]

--- Scripts/Counter.gd
+++ Scripts/Counter.gd
@@ -4,12 +4,14 @@ extends PanelContainer
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$"HBoxContainer/Money Output".text = str(Globals.cash)
-	$HBoxContainer/CPS.text = str(Globals.Cash_per_Second)
+	$HBoxContainer/VBoxContainer/CPS.text = str(Globals.Cash_per_Second)
+	$HBoxContainer/VBoxContainer/Clicks.text = str(Globals.Clicks_per_Second)
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	$"HBoxContainer/Money Output".text = "$" + str(Globals.cash)
-	$"HBoxContainer/CPS".text = "Cash per second:" + str(Globals.Cash_per_Second)
+	$"HBoxContainer/VBoxContainer/CPS".text = "Cash per second:" + str(Globals.Cash_per_Second)
+	$HBoxContainer/VBoxContainer/Clicks.text = "Clicks per second:" + str(Globals.Clicks_per_Second)
 	pass
--- Scripts/cash_effect.gd
+++ Scripts/cash_effect.gd
@@ -5,7 +5,7 @@ func _ready() -> void:
 	amount = Globals.Clicks_per_Second
 	emitting = true
 	rotation = 90
-	position.x = randi_range(110,550)
+	position.x = randi_range(130,580)
 	position.y = 0
 	#gives the effect a lifespan by adding a timer then deleting it after its done
 	await get_tree().create_timer(lifetime).timeout
