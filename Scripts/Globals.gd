2cea72db83679c3acf00bdb192393c1fb8333879
Aiden <aidenbuttinez@gmail.com>
2025-10-14 15:42:58 -0700

    Actual v1.5
    Added a basic menu, shop scene, and audio files :].

--- Scripts/Globals.gd
+++ Scripts/Globals.gd
@@ -1,7 +1,7 @@
 extends Node
 
 var cash :int = 0
-var Clicks_per_Second :int = 1
+var Clicks_per_Second :int = 1000
 var Cash_per_Second :int = 0
 
 #scenes
--- Scripts/test_clicker.gd
+++ Scripts/test_clicker.gd
@@ -2,8 +2,18 @@ extends TextureButton
 
 var CashFX = Globals.Casheffect
 
-func _on_pressed() -> void:
+func Cash_effect():
 	Globals.cash += Globals.Clicks_per_Second
 	var I_CashFX = CashFX.instantiate()
 	$"..".add_child(I_CashFX)
+pass
+
+func Cash_Soundeffect():
+	$"../AudioStreamPlayer".pitch_scale = randi_range(-1,2)
+	$"../AudioStreamPlayer".play()
+pass
+
+func _on_pressed() -> void:
+	Cash_effect()
+	Cash_Soundeffect()
 	pass # Replace with function body.
