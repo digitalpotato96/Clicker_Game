3c8e296eb751cdcad91dadded605b1525d3ed755
Aiden <aidenbuttinez@gmail.com>
2025-10-14 12:33:59 -0700

    v1.5
    Added bottom menu and buttons, as well as place holder art for said buttons.
    also added clicker scene for clicker only parts and related scene parts.

---
+++ Scenes/Menus_Interactions.gd
@@ -0,0 +1,22 @@
+extends PanelContainer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+#opens the theme menu when the button is pressed.
+func _on_custom_toggled(toggled_on: bool) -> void:
+	pass # Replace with function body.
+
+#triggers a transition to the shop scene when the button is pressed.
+func _on_shop_pressed() -> void:
+	pass # Replace with function body.
+
+#opens the settings menu when the button is pressed.
+func _on_settings_pressed() -> void:
+	pass # Replace with function body.
